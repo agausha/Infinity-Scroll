45fb4ee5580699b954118e938585c9199cb2a8d9
Isaac <uniqucode@gmail.com>
2022-07-27 07:58:08 +0100

    Added function

--- js/script.js
+++ js/script.js
@@ -12,3 +12,5 @@ let isInitialLoad = true;
 let picCount = 5;
 const apiKey = 'NIlze4okNpOKbHbn0mD1b6j3HdLuWyM8_MrkJwCkJIw';
 let apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${picCount}`;
+
+function updateAPIURLWithNewCount (picCount) {}
