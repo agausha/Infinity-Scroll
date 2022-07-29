7417a362ec2d4730a4a4e2faef8dc798a9e509ee
Isaac <uniqucode@gmail.com>
2022-07-29 09:41:16 +0100

    Checking and loading more photos

--- js/script.js
+++ js/script.js
@@ -76,7 +76,9 @@ async function getPhotos() {
 
 // Check to see if scrolling near bottom of page, Load More Photos
 window.addEventListener('scroll', () => {
-  
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+  }
 })
 
 // On Load
