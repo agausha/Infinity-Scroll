032cbfe769441e79652c496e775c28aec7c38ffc
Isaac <uniqucode@gmail.com>
2022-09-16 16:03:54 +0100

    Update comment

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-// Variables
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
